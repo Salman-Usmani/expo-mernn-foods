a04434a7c4ce920dda08331f16f97bc46137f738
Salman Usmani <salman090898@gmail.com>
2026-01-01 23:21:55 +0500

    removed stuff

--- mernn_foods/app/index.tsx
+++ mernn_foods/app/index.tsx
@@ -1,33 +1,29 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useSession } from '@/context/ctx';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Stack } from 'expo-router';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useSession } from "@/context/ctx";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Stack } from "expo-router";
 
 const index = () => {
-    const { session } = useSession();
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    console.log('session', session)
-    return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            {/* <Text style={{fontSize: 160}}>slo</Text> */}
-            <Stack>
-                {/* {session ?
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {/* <Text style={{fontSize: 160}}>slo</Text> */}
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: true }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      {/* <StatusBar style="auto" /> */}
+    </ThemeProvider>
+  );
+};
 
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    :
+export default index;
 
-                    } */}
-                    <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-            </Stack>
-            {/* <StatusBar style="auto" /> */}
-        </ThemeProvider>
-    )
-}
-
-export default index
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- mernn_foods/app/sign-in.tsx
+++ mernn_foods/app/sign-in.tsx
@@ -1,33 +1,19 @@
-import { router } from 'expo-router';
-import { Text, View } from 'react-native';
-
-import { useSession } from '@/context/ctx';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
-
-
+import { router } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function SignIn() {
-  const { signIn, isLoading } = useSession();
-  console.log('first')
+  console.log("first");
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <GoogleSigninButton size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        disabled={isLoading}
-         onPress={signIn} />
-      {/* <Text
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text
         onPress={() => {
-          signIn();
           // Navigate after signing in. You may want to tweak this to ensure sign-in is
           // successful before navigating.
-          router.replace('/');
-        }}>
+          router.replace("/");
+        }}
+      >
         Sign In
-      </Text> */}
+      </Text>
     </View>
   );
 }
